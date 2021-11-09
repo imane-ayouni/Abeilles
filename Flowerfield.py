82019753245f20c1129c05413ebaeb88a68cced6
imane <imaneayouni@outlook.com>
2021-11-09 16:03:29 +0100

    in progress

--- Flowerfield.py
+++ Flowerfield.py
@@ -2,10 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 from random import sample
+import math
 
 flowerField = pd.read_excel(r'C:\Users\imane\OneDrive\Desktop\Abeilles\flowers.xlsx')
 x = list(flowerField['x'])
 y = list(flowerField['y'])
+
 
 flowerPositions = {x[i]: y[i] for i in range(len(x))}
 fpos = []
@@ -56,13 +58,39 @@ for i in range(n_population-1):
     plist = sample(list(flower_list.values()), len(flower_list.values()))
     path.append(plist)
 
-print(len(path))
+
+# Obtenir un dict qui contient le chemin parcourus par chaque abeille
+
 path_dict = {}
-for fl in range(1,len(flower_list.keys())+1):
-    pa = {fl: path[fl]}
+for bee in range(n_population-1):
+    pa = {str(bee+1): path[bee]}
     path_dict.update(pa)
 for u in path_dict:
     print(u,path_dict[u],"\n")
+
+distances = []
+def getDistance():
+
+    for bee in range(n_population-1):
+
+        p = path[bee]
+        exit_distance = math.sqrt((p[0][0]-500)**2 +(p[0][1] - 500)**2)
+        dis = []
+        for fl in range(len(p)-1):
+            distance = math.sqrt((p[fl+1][0]-p[fl][0])**2 + (p[fl+1][1] - p[fl][1])**2)
+            dis.append(distance)
+        in_nodes_distance = sum(dis)
+        return_distance = math.sqrt((500 - p[49][0])**2 + (500 - p[49][1])**2)
+        total_distance = exit_distance + in_nodes_distance + return_distance
+        distances.append(total_distance)
+
+getDistance()
+print(len(distances))
+print(distances)
+
+
+
+
 
 
 
