6dc5484095955879e9d7d0c1852dbbccb90b85a9
imane <imaneayouni@outlook.com>
2021-11-09 22:21:46 +0100

    in progress

--- Flowerfield.py
+++ Flowerfield.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -68,6 +70,9 @@ for bee in range(n_population-1):
 for u in path_dict:
     print(u,path_dict[u],"\n")
 
+# Calculer les distances parcourus par chaque abeille depuis le point de départ (hive) passant
+# par toutes les fleures et revenant en fin au hive
+
 distances = []
 def getDistance():
 
@@ -84,9 +89,56 @@ def getDistance():
         total_distance = exit_distance + in_nodes_distance + return_distance
         distances.append(total_distance)
 
+
 getDistance()
 print(len(distances))
 print(distances)
+
+
+def selection(population,scores,sample = 3):
+    first_selection = random.randint(len(population))
+    for selec in random.randint(0,len(population),sample-1):
+        if scores[selec] < scores[first_selection]:
+            first_selection = selec
+    return population[first_selection]
+
+
+
+def crossover(parent_1,parent_2,p_crossover):
+    child_1,child_2 = parent_1.copy(), parent_2.copy()
+    if random.random() < p_crossover:
+        point = random.randint(1,len(parent_1)-2)
+        child_1 = parent_1[:point] + parent_2[point:]
+        child_2 = parent_2[:point] + parent_1[point:]
+    return [child_1,child_2]
+
+r_mutation = 2.2* 10**(-9)
+
+def mutation(bitstring,r_mutation):
+    for i in range(len(bitstring)):
+        if random.random() > r_mutation:
+            bitstring[i] = 1 - bitstring[i]
+
+
+
+def reproduction(fitness,genes,generations,n_population,p_crossover,r_mutation):
+    population = [random.randint(0,2,genes).tolist() for _ in range(n_population)]
+    for generation in range(generations):
+        scores = [fitness(bee) for bee in population]
+        selected = [selection(population,scores) for _ in range(n_population)]
+        children = list()
+        for i in range(0,n_population,2):
+            parent_1,parent_2 = selected[i],selected[i+1]
+            for child in crossover(parent_1,parent_2,p_crossover):
+                mutation(child,r_mutation)
+                children.append(child)
+
+        population = children
+
+
+
+
+
 
 
 
